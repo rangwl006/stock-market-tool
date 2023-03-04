1fbcad1251dd1c730926d657aae266d209c07e93
Russell Ang <rangwl006@gmail.com>
2023-03-04 13:22:32 +0800

    encapsulate moving average calculation methods into policy structs, modified unit tests

--- inc/modules/utils/statistics/MovingAverage.h
+++ inc/modules/utils/statistics/MovingAverage.h
@@ -8,28 +8,64 @@ namespace utils
     template
     <
       template <typename> class Container = std::vector,
-      typename ValueType
+      typename ValueType = double
     >
-    std::vector<ValueType> CalculateSimpleMovingAverage(std::vector<ValueType> values, size_t windowSize)
+    struct SMACalculator
     {
-        std::vector<ValueType> output(windowSize - 1);
-        assert(output.size() == windowSize - 1);
-        std::cout << "Created output with default window size: " << output.size() << std::endl;
-        int left = 0;
-        ValueType currSum = 0;
-
-        for_each(values.begin(), values.begin() + windowSize, [&currSum](ValueType v){ currSum += v; } );
-        output.push_back(currSum / windowSize);
-
-        for(int right = windowSize; right < values.size(); right++)
+        static std::vector<ValueType> CalculateSimpleMovingAverage(std::vector<ValueType> values, size_t windowSize)
         {
-            currSum -= values[left];
-            currSum += values[right];
+            std::vector<ValueType> output(windowSize - 1);
+            assert(output.size() == windowSize - 1);
+            std::cout << "Created output with default window size: " << output.size() << std::endl;
+            int left = 0;
+            ValueType currSum = 0;
+
+            for_each(values.begin(), values.begin() + windowSize, [&currSum](ValueType v){ currSum += v; } );
             output.push_back(currSum / windowSize);
-            left++;
-        }
-        return output;
+
+            for(int right = windowSize; right < values.size(); right++)
+            {
+                currSum -= values[left];
+                currSum += values[right];
+                output.push_back(currSum / windowSize);
+                left++;
+            }
+            return output;
+        };
     };
-    // template<template<typename Container> class Value>
-    // Value CalculateExponentialMovingAverage(Container<Value> values, size_t windowSize);
+
+    /**
+     * Formula: EMA = K * (Current price - Previous EMA) + Previous EMA
+     * where K = 2 / (n + 1) is the weighting factor, n = time period
+    */
+    template
+    <
+      template <typename> class Container = std::vector,
+      typename ValueType = double
+    >
+    struct EMACalculator
+    {
+        std::vector<ValueType> CalculateExponentialMovingAverage(std::vector<ValueType>& values, int period)
+        {
+            // calculate the weighting 
+            double k = 2.0 / ( period + 1 );
+            std::cout << "k: " << k << std::endl;
+
+            // set initial ema to first value
+            ValueType currEma = values[0];
+    
+            std::vector<ValueType> output{};
+            output.push_back(currEma);
+
+            for(int i = 1; i < values.size(); i++)
+            {
+                currEma = currEma + k * (values[i] - currEma);
+                std::cout << "values[" << i << "]: " << values[i] << std::endl;
+                std::cout << "ema(" << i << "): " << currEma << std::endl;
+                output.push_back(currEma);
+            }
+
+            return output;
+        };
+    };
 }
--- test/MovingAverageTest.cpp
+++ test/MovingAverageTest.cpp
@@ -1,11 +1,17 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+
 #include "MovingAverage.h"
 
-TEST_CASE("Test Catch2")
+using Catch::Approx;
+using namespace Catch::literals;
+
+TEST_CASE("Test Simple Moving Average")
 {
     std::vector<float> v{1,2,3,4,5};
     int windowSize = 3;
-    auto res = utils::CalculateSimpleMovingAverage(v, windowSize);
+    auto smaCalculator = utils::SMACalculator<std::vector, float>();
+    auto res = smaCalculator.CalculateSimpleMovingAverage(v, windowSize);
     float x = (1 + 2 + 3) / 3;
     float y = (2 + 3 + 4) / 3;
     float z = (3 + 4 + 5) / 3;
@@ -19,5 +25,14 @@ TEST_CASE("Test Catch2")
     REQUIRE(x == res[2]);
     REQUIRE(y == res[3]);
     REQUIRE(z == res[4]);
+}
 
+TEST_CASE("Test Exponential Moving Average")
+{
+    std::vector<float> v{1,2,3,4,5,6,7,8,9,10};
+    int period = 3;
+    auto emaCalculator = utils::EMACalculator<std::vector, float>();
+    auto res = emaCalculator.CalculateExponentialMovingAverage(v, period);
+    
+    REQUIRE(res[0] == 1);
 }
